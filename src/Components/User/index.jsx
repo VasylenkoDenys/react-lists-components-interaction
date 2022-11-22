24e682633f957cbd12a5ae6c15f9a81935a4ff37
VasylenkoDenys <vasylenkodenys92@gmail.com>
2022-11-22 19:50:26 +0200

    liker+delete

--- src/Components/User/index.jsx
+++ src/Components/User/index.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 
 class User extends React.Component {
-
-  render(){
-    const {id, firstName, lastName, likes, isLiked, liker} = this.props;
-    console.log(firstName)
-    return ( 
+  render() {
+    const {id, firstName, lastName, likes, isLiked, liker, deleteUser } =
+      this.props;
+    return (
       <div className="userCard">
         <h2>User info</h2>
         <p>First name: {firstName}</p>
         <p>Last name: {lastName}</p>
         <p>Likes: {likes}</p>
-        <button className="btn" onClick={liker(id)} hidden={isLiked}>like</button>
-        <button >delete user</button>
-
+        <button className="btn" onClick={()=>liker(id)} hidden={isLiked}>
+          like
+        </button>
+        <button className="btn" onClick={()=>deleteUser(id)}>
+          delete user
+        </button>
       </div>
     );
-    }
   }
+}
 
-  export default User;
+export default User;
--- src/Components/UsersList/index.jsx
+++ src/Components/UsersList/index.jsx
@@ -1,35 +1,61 @@
-import React, { Component } from 'react';
-import User from '../User';
+import React, { Component } from "react";
+import User from "../User";
 
 class UsersList extends Component {
-
-  state = {users:[
-    {id: 1, firstName: 'Vasya', lastName: 'Pupkin', likes: 0, isLiked: false,},
-    {id: 2, firstName: 'Anna', lastName: 'Gorbenko', likes: 0, isLiked: false,},
-  ]};
-
-  mapUsers= (user)=>{
-    return (
-    <User firstName={user.firstName} lastName={user.lastName} key={user.id} likes= {user.likes}/>
-  )};
-
-  liker=()=> {
-    // const {id, likes, isLiked} = this.state;
-    this.setState({users: this.state.users.map((user) =>{
-      return{...user,
-        likes: user.id ? user.likes+1:user.likes,
-        isLiked: user.id ? user.isLiked === true : user.isLiked,}
-  }),
-});
+  state = {
+    users: [
+      {
+        id: 1,
+        firstName: "Vasya",
+        lastName: "Pupkin",
+        likes: 0,
+        isLiked: false,
+      },
+      {
+        id: 2,
+        firstName: "Anna",
+        lastName: "Gorbenko",
+        likes: 0,
+        isLiked: false,
+      },
+    ],
   };
+
+  mapUsers = (user) => {
+    return (
+      <User
+        firstName={user.firstName}
+        lastName={user.lastName}
+        key={user.id}
+        likes={user.likes}
+      />
+    );
+  };
+  
+  liker=(id)=> {
+    this.setSate({
+      users: this.state.users.map((user)=>{
+        return {
+          ...user,
+          likes: id === user.id ? user.likes +1 : user.likes,
+        };
+      }),
+    });
+  };
+
+  deleteUser=(id)=> {
+    return(
+    this.setState({
+      users: this.state.users.filter((user) => id !== user.id),
+    })
+  )};
   render() {
-    const {users} = this.state;
+    const { users } = this.state;
     const usersList = users.map(this.mapUsers);
     return (
       <div>
-        <h1 style={{textAlign: 'center'}}>Users</h1>
+        <h1 style={{ textAlign: "center" }}>Users</h1>
         <article>{usersList}</article>
-
       </div>
     );
   }
